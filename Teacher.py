3f2ad555df4850a7a20a3ec3d63f0b4d23a91bf3
lijianqiang <37371278+OnlyLoveLin@users.noreply.github.com>
2018-04-10 21:11:28 +0800

    Create Teacher.py

---
+++ Teacher.py
@@ -0,0 +1,11 @@
+# 这是一个教师类
+class Teacher:
+    # 初始化属性
+    def __init__(self,teacher_id,teacher_password,teacher_name,teacher_age,teacher_sex,teacher_address,teacher_major):
+        self.teacher_id = teacher_id
+        self.teacher_password = teacher_password
+        self.teacher_name = teacher_name
+        self.teacher_age = teacher_age
+        self.teacher_sex = teacher_sex
+        self.teacher_adress = teacher_address
+        self.teacher_major = teacher_major
