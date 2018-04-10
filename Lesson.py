d91c50368c9a1b953bd80d82892d73171cc82396
lijianqiang <37371278+OnlyLoveLin@users.noreply.github.com>
2018-04-10 21:09:03 +0800

    Create Lesson.py

---
+++ Lesson.py
@@ -0,0 +1,9 @@
+# 这是一个课程类
+
+class Lesson:
+    # 初始化属性
+    def __init__(self,lesson_name,lesson_tercher,lesson_num,lesson_time):
+        self.lesson_name = lesson_name
+        self.lesson_teacher = lesson_tercher
+        self.lesson_num = lesson_num
+        self.lesson_time = lesson_time
