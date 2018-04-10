d8dcf64c3e12035e1661724d7aa354b552a6ba11
lijianqiang <37371278+OnlyLoveLin@users.noreply.github.com>
2018-04-10 21:11:10 +0800

    Create StuGrade.py

---
+++ StuGrade.py
@@ -0,0 +1,10 @@
+# 这是一个学生成绩类
+
+class StuGrade:
+    # 初始化属性
+    def __init__(self,stuId,stuName,stuLesson,stuGrade,stuMajor):
+        self.stuId = stuId
+        self.stuName = stuName
+        self.stuLesson = stuLesson
+        self.stuGrade = stuGrade
+        self.stuMajor = stuMajor
